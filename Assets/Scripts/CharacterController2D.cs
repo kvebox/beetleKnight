e8b7f8910a2eb3b37de78dbce9e891376acf71ed
5e3c8265f2f3540c8c817eb0 <victoria@pared.com>
2021-09-10 00:16:30 -0700

    add dash capability

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -3,11 +3,7 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour {
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
-    [SerializeField] private float m_JumpForce = 600f;
-    [SerializeField] private float m_DashForce = 100f;
     [SerializeField] private bool m_isGrounded = true;
-    [SerializeField] private bool m_isDashing = false;
-    [SerializeField] private bool m_AirControl = true;
     [SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private bool m_DirectionRight = true;
@@ -24,13 +20,11 @@ public class CharacterController2D : MonoBehaviour {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
 
         if (OnLandEvent == null) OnLandEvent = new UnityEvent();
-		// if (OnCrouchEvent == null) OnCrouchEvent = new BoolEvent();
     }
 
     private void FixedUpdate() {
         bool wasGrounded = m_isGrounded;
         m_isGrounded = false;
-        // m_isDashing = false;
 
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, m_GroundedRadius, m_WhatIsGround);
 		for (int i = 0; i < colliders.Length; i++) {
@@ -41,28 +35,20 @@ public class CharacterController2D : MonoBehaviour {
 		}
     }
 
-    public void Move(float move, bool jump, bool dash) {
-        if (m_isGrounded || m_AirControl) {
-            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    public void Move(float move, float jump, bool dash) {
+        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
-            if (move > 0 && !m_DirectionRight) Flip();
-            else if (move < 0 && m_DirectionRight) Flip();
-        }
+        if (move > 0 && !m_DirectionRight) Flip();
+        else if (move < 0 && m_DirectionRight) Flip();
 
-        if (jump && m_isGrounded) {
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        if (jump > 0 && m_isGrounded) {
+            m_Rigidbody2D.AddForce(new Vector2(0f, jump));
             m_isGrounded = false;
-        }
-
-        if (dash) {
-            m_Rigidbody2D.AddForce(new Vector2(m_DashForce, 0f));
-            m_isDashing = true;
         }
     }
 
     public void Flip() {
         m_DirectionRight = !m_DirectionRight;
-        m_DashForce *= -1;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,27 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed = 25f;
+    public float speed = 50f;
+    public float jump = 800f;
+    public float dashSpeed = 250f;
+    public float dashCountThreshold = 225;
+    public float dashCooldown = 25;
+    public float dashTimer = 0;
+    private float currentDashSpeed;
+
     public CharacterController2D controller;
     float horizontalMove = 0f;
-    bool isJumping = false;
+    float isJumping;
+    float direction = 0;
     bool isDashing = false;
 
+    public enum State {
+        NORMAL,
+        DASH,
+    }
+
+    private State state;
+
+    void Start() {
+        state = State.NORMAL;
+    }
+
     void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        if (horizontalMove != 0) direction = horizontalMove;
+
         if (Input.GetButtonDown("Jump")){
-            isJumping = true;
+            isJumping = jump;
         }
 
-        if (Input.GetButtonDown("Dash")){
-            isDashing = true;
+        switch (state) {
+            case State.NORMAL:
+                if (Input.GetButtonDown("Dash")){
+                    if (dashTimer <= 0) {
+                        currentDashSpeed = dashSpeed;
+                        dashTimer = dashCooldown;
+                        state = State.DASH;
+                        isDashing = true;
+                    }
+                }
+                break;
+            
+            case State.DASH:
+                currentDashSpeed -= currentDashSpeed * Time.deltaTime;
+                horizontalMove += direction < 0 ? -currentDashSpeed : currentDashSpeed;
+
+                if (currentDashSpeed < dashCountThreshold) {
+                    state = State.NORMAL;
+                    isDashing = false;
+                    dashTimer = dashCooldown;
+                }
+                break;
         }
-        
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
     }
 
     void FixedUpdate() {
+        dashTimer--;
         controller.Move(horizontalMove * Time.fixedDeltaTime, isJumping, isDashing);
-        isJumping = false;
-        isDashing = false;
+        isJumping = 0;
     }
 }
